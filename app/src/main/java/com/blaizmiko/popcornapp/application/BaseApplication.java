3471f486be75a2343cf5db0d8c44e80c60867ed4
IstrajI <Mr.Straj@yandex.ru>
2017-02-05 20:46:20 +0300

    Change gson dependency. Add getRetrofit method

--- app/src/main/java/com/blaizmiko/popcornapp/application/BaseApplication.java
+++ app/src/main/java/com/blaizmiko/popcornapp/application/BaseApplication.java
@@ -2,10 +2,42 @@ package com.blaizmiko.popcornapp.application;
 
 import android.app.Application;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit.GsonConverterFactory;
+import retrofit2.Retrofit;
+
 public class BaseApplication extends Application{
+
+    private static final String BASE_URL = "";
+    private static final Retrofit retrofit = null;
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+
+        }
+        return retrofit;
+    }
+    }
+
+    public static Retrofit getRetrofit() {
+        if (retrofit!= null) return retrofit;
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
     }
 }
