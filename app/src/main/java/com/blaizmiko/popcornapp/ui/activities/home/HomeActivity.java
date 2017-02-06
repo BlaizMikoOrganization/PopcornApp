b73b8190d53c2328cc2038d14b56a02c94336d1d
Uladzislau Nikitsin <Uladzislau_Nikitsin@epam.com>
2017-02-06 18:45:19 +0300

    Add test version of main navigation screen

---
+++ app/src/main/java/com/blaizmiko/popcornapp/ui/activities/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.blaizmiko.popcornapp.ui.activities.home;
+
+import android.os.Bundle;
+
+import com.blaizmiko.popcornapp.R;
+import com.blaizmiko.popcornapp.ui.activities.base.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/blaizmiko/popcornapp/ui/activities/splash/SplashActivity.java
+++ app/src/main/java/com/blaizmiko/popcornapp/ui/activities/splash/SplashActivity.java
@@ -1,13 +1,19 @@
 package com.blaizmiko.popcornapp.ui.activities.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.blaizmiko.popcornapp.ui.activities.base.BaseActivity;
+import com.blaizmiko.popcornapp.ui.activities.home.HomeActivity;
 
 public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
