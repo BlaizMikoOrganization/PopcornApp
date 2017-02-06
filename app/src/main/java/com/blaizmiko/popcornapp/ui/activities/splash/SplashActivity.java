eeaf624ab76531f4568892bf1cc770b6d3ea2fff
Uladzislau Nikitsin <Uladzislau_Nikitsin@epam.com>
2017-02-06 16:57:07 +0300

    feature/#3_Add_splash_screen

--- app/src/main/java/com/blaizmiko/popcornapp/ui/activities/splash/SplashActivity.java
+++ app/src/main/java/com/blaizmiko/popcornapp/ui/activities/splash/SplashActivity.java
@@ -4,10 +4,6 @@ import android.os.Bundle;
 
 import com.blaizmiko.popcornapp.ui.activities.base.BaseActivity;
 
-/**
- * Created by Uladzislau_Nikitsin on 06.02.2017.
- */
-
 public class SplashActivity extends BaseActivity {
 
     @Override
