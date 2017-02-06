ab494a7f2b1ff6296000b9539933a25b2d08866f
Uladzislau Nikitsin <Uladzislau_Nikitsin@epam.com>
2017-02-06 14:38:27 +0300

    Add splash screen

---
+++ app/src/main/java/com/blaizmiko/popcornapp/ui/activities/splash/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.blaizmiko.popcornapp.ui.activities.splash;
+
+import android.os.Bundle;
+
+import com.blaizmiko.popcornapp.ui.activities.base.BaseActivity;
+
+/**
+ * Created by Uladzislau_Nikitsin on 06.02.2017.
+ */
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
