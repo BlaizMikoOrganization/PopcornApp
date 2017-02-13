abb9e7e74b766ba20b640a588f42f478286ef32f
IstrajI <Mr.Straj@yandex.ru>
2017-02-13 17:51:20 +0300

    Finished on redesign of popular movies window. Closes #29

--- app/src/main/java/com/blaizmiko/popcornapp/models/Movie/ShortMovie.java
+++ app/src/main/java/com/blaizmiko/popcornapp/models/Movie/ShortMovie.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class ShortMovie extends BaseMovie{
+public class ShortMovie extends com.blaizmiko.popcornapp.models.movie.BaseMovie {
     @SerializedName("genre_ids")
     private List<Integer> mGenreIds;
 
