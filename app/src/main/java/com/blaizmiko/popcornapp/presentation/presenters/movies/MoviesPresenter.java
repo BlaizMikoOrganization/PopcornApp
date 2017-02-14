28bfcaed29cfeaf5888274fb4f8fe53dfcf87e65
IstrajI <Mr.Straj@yandex.ru>
2017-02-14 11:48:44 +0300

    Removed created by comment. Closes #27

--- app/src/main/java/com/blaizmiko/popcornapp/presentation/presenters/movies/MoviesPresenter.java
+++ app/src/main/java/com/blaizmiko/popcornapp/presentation/presenters/movies/MoviesPresenter.java
@@ -3,10 +3,6 @@ package com.blaizmiko.popcornapp.presentation.presenters.movies;
 import com.blaizmiko.popcornapp.presentation.presenters.base.BaseMvpPresenter;
 import com.blaizmiko.popcornapp.presentation.views.movies.MoviesView;
 
-/**
- * Created by Uladzislau_Nikitsin on 14.02.2017.
- */
-
 public class MoviesPresenter extends BaseMvpPresenter<MoviesView> {
 
 }
