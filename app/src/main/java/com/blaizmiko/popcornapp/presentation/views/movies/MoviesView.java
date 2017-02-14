a196f708ba4f5a0db5a1c4432eb11920fc7919df
IstrajI <Mr.Straj@yandex.ru>
2017-02-14 11:44:33 +0300

    Updated Movies fragment based on moxy mvp

---
+++ app/src/main/java/com/blaizmiko/popcornapp/presentation/views/movies/MoviesView.java
@@ -0,0 +1,7 @@
+package com.blaizmiko.popcornapp.presentation.views.movies;
+
+import com.arellomobile.mvp.MvpView;
+import com.blaizmiko.popcornapp.models.actors.PopularActors;
+
+public interface MoviesView extends MvpView {
+}
--- app/src/main/java/com/blaizmiko/popcornapp/ui/fragments/movies/MoviesFragment.java
+++ app/src/main/java/com/blaizmiko/popcornapp/ui/fragments/movies/MoviesFragment.java
@@ -8,10 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.blaizmiko.popcornapp.R;
 import com.blaizmiko.popcornapp.models.movie.ShortMovie;
+import com.blaizmiko.popcornapp.presentation.presenters.movies.MoviesPresenter;
+import com.blaizmiko.popcornapp.presentation.views.movies.MoviesView;
 import com.blaizmiko.popcornapp.ui.adapters.ShortMoviesAdapter;
-import com.blaizmiko.popcornapp.ui.fragments.base.BaseFragment;
+import com.blaizmiko.popcornapp.ui.fragments.base.BaseMvpFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +22,10 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MoviesFragment extends BaseFragment {
+public class MoviesFragment extends BaseMvpFragment implements MoviesView {
+
+    @InjectPresenter
+    MoviesPresenter mMoviesPresenter;
 
     //Bind views
     @BindView(R.id.fragment_movies_recycler_view)
@@ -42,10 +48,10 @@ public class MoviesFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_movies, container, false);
         ButterKnife.bind(this, view);
 
-        int testMoviesAmount = 6;
-        ShortMoviesAdapter adapter = new ShortMoviesAdapter(generateTestMovies(testMoviesAmount), getContext());
+        final int testMoviesAmount = 6;
+        final ShortMoviesAdapter adapter = new ShortMoviesAdapter(generateTestMovies(testMoviesAmount), getContext());
 
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        final RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setAdapter(adapter);
@@ -54,9 +60,7 @@ public class MoviesFragment extends BaseFragment {
     }
 
     @Override
-    protected void bindViews() {
-
-    }
+    protected void bindViews() {}
 
     //Generate testMovie List
     private List<ShortMovie> generateTestMovies(int testMoviesAmount) {
