7192ccf296f4723c099a9db86b9c63ca61b76b73
Mikhail_Kantsavoi <mikhail_kantsavoi@epam.com>
2017-04-12 13:19:36 +0300

    Small fix

--- app/src/main/java/com/blaizmiko/popcornapp/ui/all/activities/BaseDetailsActivity.java
+++ app/src/main/java/com/blaizmiko/popcornapp/ui/all/activities/BaseDetailsActivity.java
@@ -1,36 +1,27 @@
 package com.blaizmiko.popcornapp.ui.all.activities;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.blaizmiko.popcornapp.R;
 import com.blaizmiko.popcornapp.application.Constants;
-import com.blaizmiko.popcornapp.ui.all.activities.BaseMvpActivity;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import butterknife.BindView;
 
 public abstract class BaseDetailsActivity extends BaseMvpActivity {
+
     //Bind views
     @BindView(R.id.toolbar_details_toolbar)
     protected Toolbar toolbar;
     @BindView(R.id.image_view_details_toolbar_backdrop)
     protected ImageView backdropImageView;
-    @BindView(R.id.image_view_details_toolbar_poster)
-    protected ImageView posterImageView;
     @BindView(R.id.viewpager_details)
     protected ViewPager viewPager;
     @BindView(R.id.progress_bar_details_load)
@@ -42,7 +33,7 @@ public abstract class BaseDetailsActivity extends BaseMvpActivity {
     protected String cinemaName;
     protected double rating;
 
-    //Life Cycle Methods
+    //Life cycle
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,27 +41,21 @@ public abstract class BaseDetailsActivity extends BaseMvpActivity {
     }
 
     protected void bindToolbar() {
-
         rating = getIntent().getDoubleExtra(Constants.Extras.RATING, Constants.MovieDbApi.DEFAULT_CINEMA_RATING);
         id = getIntent().getIntExtra(Constants.Extras.ID, Constants.MovieDbApi.DEFAULT_CINEMA_ID);
         cinemaName = getIntent().getStringExtra(Constants.Extras.TITLE);
 
-        toolbar.setTitle(cinemaName);
         setToolbar(toolbar);
+        setToolbarTitle(cinemaName);
         setToolbarDisplayHomeButtonEnabled(true);
 
-        Context context = getApplication().getApplicationContext();
-        Glide.with(context)
-                .load(Constants.MovieDbApi.BASE_HIGH_RES_IMAGE_URL + getIntent().getStringExtra(Constants.Extras.POSTER_URL))
-                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                .into(posterImageView);
-        Glide.with(context)
+        Glide.with(getApplicationContext())
                 .load(Constants.MovieDbApi.BASE_HIGH_RES_IMAGE_URL + getIntent().getStringExtra(Constants.Extras.BACKDROP_URL))
                 .diskCacheStrategy(DiskCacheStrategy.SOURCE)
                 .into(backdropImageView);
     }
 
-    //Callbacks
+    //Listeners
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
         switch (item.getItemId()) {
