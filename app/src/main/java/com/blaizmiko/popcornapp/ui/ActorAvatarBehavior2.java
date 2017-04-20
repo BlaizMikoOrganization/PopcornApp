28ad240ad039c08309a09d32e94ff24c2318be3b
IstrajI <Mr.Straj@yandex.ru>
2017-04-20 16:58:39 +0300

    Almost finished detals actor's toolbar

--- app/src/main/java/com/blaizmiko/popcornapp/ui/ActorAvatarBehavior2.java
+++ app/src/main/java/com/blaizmiko/popcornapp/ui/ActorAvatarBehavior2.java
@@ -1,12 +1,16 @@
-package com.blaizmiko.ui;
+package com.blaizmiko.popcornapp.ui;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.Window;
+
+import com.blaizmiko.popcornapp.ui.actors.details.DetailsActorActivity;
 
 import org.androidannotations.annotations.App;
 
@@ -41,19 +45,20 @@ public class ActorAvatarBehavior2 extends CoordinatorLayout.Behavior<CircleImage
 
 
     private float toolbarHeight;
+    private float backButtonWidth;
     private boolean wasInitiated = false;
 
     private float startToolbarYPosition;
     private float changeBehaviorPoint;
 
-    private int finalPictureSize = 25;
+    private int finalPictureSize = 32;
     private int finalPictureXMargin = 36;
     private int finalPictureYMargin = 16;
 
 
     private float startPictureXPosition = 16;
-    private float finalPictureXPosition = 36;
-    private float finalPictureYPosition = 16;
+    private float finalPictureXPosition = 40;
+    private float finalPictureYPosition = 0;
     private float startPictureYPosition;
     private int startPictureSize;
 
@@ -64,7 +69,11 @@ public class ActorAvatarBehavior2 extends CoordinatorLayout.Behavior<CircleImage
         TypedValue tv = new TypedValue();
         if (mContext.getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
              toolbarHeight = TypedValue.complexToDimensionPixelSize(tv.data, mContext.getResources().getDisplayMetrics());
+        }
 
+        if (mContext.getTheme().resolveAttribute(android.R.attr.homeAsUpIndicator, tv, true)) {
+            backButtonWidth = TypedValue.complexToDimensionPixelSize(tv.data, mContext.getResources().getDisplayMetrics());
+            System.out.println("backButtonWidth " +backButtonWidth);
         }
 
         startPictureXPosition = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, startPictureXPosition , mContext.getResources().getDisplayMetrics());
@@ -87,113 +96,90 @@ public class ActorAvatarBehavior2 extends CoordinatorLayout.Behavior<CircleImage
     public boolean onDependentViewChanged(CoordinatorLayout parent, CircleImageView child, View dependency) {
         initStartValues(child, dependency);
 
-
-        float toolbarPassPercentOfHeight =(startToolbarYPosition - dependency.getBottom())/ startToolbarYPosition;
-
-        child.setX(startPictureXPosition + (finalPictureXPosition - startPictureXPosition) * toolbarPassPercentOfHeight);
-        child.setY(finalPictureYPosition + (finalPictureYPosition - startPictureYPosition) * toolbarPassPercentOfHeight);
-
-        System.out.println("child");
-        System.out.println("Y" + child.getY() + "height" + child.getHeight() + "top" + child.getTop());
-        System.out.println("dependency");
-        System.out.println("Y" + dependency.getY() + "height" + dependency.getHeight() + "top" + dependency.getTop());
-
-        return true;
-    }
-        /*
+        float finalPicY = getStatusBarHeight() + (toolbarHeight - finalPictureSize)/2;
+        float toolbarPassed = ((dependency.getBottom()- toolbarHeight - getStatusBarHeight())/(startToolbarYPosition - toolbarHeight - getStatusBarHeight()));
+        child.setY((startPictureYPosition)*toolbarPassed + finalPicY*(1-toolbarPassed));
+        child.setX(finalPictureXPosition*(1 - toolbarPassed) + startPictureXPosition);
+        //child.setY((startPictureYPosition)*toolbarPassed +getStatusBarHeight() + (toolbarHeight - finalPictureSize)/2);
 
 
-
-        child.setX(16);
-        child.setY(16);
-        System.out.println(startToolbarYPosition - dependency.getY());
-        System.out.println();
-        initStartValues(child, dependency);
-        System.out.println("");
+        CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+        lp.width = (int) (toolbarPassed * (startPictureSize - finalPictureSize) + finalPictureSize);
+        lp.height = (int) (toolbarPassed * (startPictureSize - finalPictureSize) + finalPictureSize);
+        child.setLayoutParams(lp);
 
 
-        float toolbarPassPercentOfHeight =(startToolbarYPosition - dependency.getY())/ startToolbarYPosition;
+/*        System.out.println("toolbar height " +toolbarHeight);
+        System.out.println("current toolbar y position = " +dependency.getBottom());
+        float toolbarPassPercentOfHeight =(startToolbarYPosition - (dependency.getBottom() - toolbarHeight - getStatusBarHeight()))/ startToolbarYPosition;
+        System.out.println("toolbarPass = " +toolbarPassPercentOfHeight +"   mChangeBehavior " +mChangeBehaviorPoint);
+        if (1f - toolbarPassPercentOfHeight > mChangeBehaviorPoint) {
+            System.out.println("final = " + finalPictureYPosition + " start = " + startPictureYPosition);
+            child.setX(startPictureXPosition + (finalPictureXPosition - startPictureXPosition) * toolbarPassPercentOfHeight);
+            child.setY((finalPictureYPosition) + (startPictureYPosition - finalPictureYPosition) * (1f - toolbarPassPercentOfHeight));
 
-        child.setY();
-        System.out.println("start toolbar y = " +startToolbarYPosition);
-        System.out.println("toolbar.getY = " +dependency.getY());
-        System.out.println("expecting = " +(startToolbarYPosition - (dependency.getBottom() -toolbarHeight)));
-
-        //if (1 - toolbarPassPercentOfHeight > changeBehaviorPoint) {
             System.out.println("child");
             System.out.println("Y" + child.getY() + "height" + child.getHeight() + "top" + child.getTop());
             System.out.println("dependency");
-            System.out.println("Y" + dependency.getY() + "height" + dependency.getHeight() + "top" + dependency.getTop() + "measured" + dependency.getMeasuredHeight() + "bot" + dependency.getBottom());
-
-            //maybeInitProperties(child, dependency);
-
-
-            System.out.println("toolbar pass = " + toolbarPassPercentOfHeight);
-            System.out.println("new y =" + (1f - toolbarPassPercentOfHeight) * startPictureYPosition);
-            //child.setY(finalPictureYMargin + (1f - toolbarPassPercentOfHeight) * startPictureYPosition);
-
-        System.out.println("start ");
-            System.out.println("new x =" + toolbarPassPercentOfHeight * startPictureXPosition);
-            //child.setX((1f - toolbarPassPercentOfHeight) * startPictureXPosition);
-
+            System.out.println("Y" + dependency.getY() + "height" + dependency.getHeight() + "top" + dependency.getTop());
 
             CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
-
-            System.out.println("startPictureSize = " + startPictureSize);
-            System.out.println("new size = " + (int) ((1f - toolbarPassPercentOfHeight) * startPictureSize));
             lp.width = (int) ((1f - toolbarPassPercentOfHeight) * (startPictureSize - finalPictureSize) + finalPictureSize);
             lp.height = (int) ((1f - toolbarPassPercentOfHeight) * (startPictureSize - finalPictureSize) + finalPictureSize);
             child.setLayoutParams(lp);
-        //} else {
-//            float distanceYToSubtract = ((startPictureYPosition - 0)
-//                    * (1f - toolbarPassPercentOfHeight)) + (startPictureYPosition/2);
-//            child.setX(50);
-//            child.setY(50);
-        //}
+        }
+
+            else {
+
+            System.out.println("child");
+            System.out.println("Y" + child.getY() + "height" + child.getHeight() + "top" + child.getTop());
+            System.out.println("dependency");
+            System.out.println("Y" + dependency.getY() + "height" + dependency.getHeight() + "top" + dependency.getTop());
+
+
+            child.bringToFront();
+            System.out.println("work");
+                child.setY(100);
+            child.setX(100);
+            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+            lp.width = (finalPictureSize);
+            lp.height = (finalPictureSize);
+            child.setLayoutParams(lp);
+
+            }*/
+
+
 
 
         return true;
-    }*/
-
-    private void maybeInitProperties(CircleImageView child, View dependency) {
-
-        if (mStartYPosition == 0)
-            mStartYPosition = 500;
-
-
-        if (mFinalYPosition == 0)
-            mFinalYPosition = (dependency.getHeight() /2);
-
-        if (mStartHeight == 0)
-            mStartHeight = child.getHeight();
-
-        if (mStartXPosition == 0)
-            mStartXPosition = (int) (child.getX() + (child.getWidth() / 2));
-
-        if (mFinalXPosition == 0)
-            mFinalXPosition = mContext.getResources().getDimensionPixelOffset(R.dimen.abc_action_bar_content_inset_material) + ((int) mCustomFinalHeight / 2);
-
-        if (mStartToolbarPosition == 0)
-            mStartToolbarPosition = dependency.getY();
-
-        if (mChangeBehaviorPoint == 0) {
-            mChangeBehaviorPoint = (child.getHeight() - mCustomFinalHeight) / (2f * (mStartYPosition - mFinalYPosition));
-        }
     }
+
+
 
     private void initStartValues(CircleImageView child, View dependency) {
 
 
         if (wasInitiated) return;
 
-        //child.setX(startPictureXPosition);
+        getStatusBarHeight();
 
 
+
+        //NEW
+        //child.setX(dependency.get);
         startToolbarYPosition = dependency.getBottom();
+        startPictureYPosition = dependency.getBottom() - child.getWidth()/2;
+        startPictureSize = child.getWidth();
+
+
+        child.setY(startPictureYPosition);
+
+        //
+
+/*
 
         child.setY(dependency.getBottom() - child.getHeight()/2);
         child.setX(startPictureXPosition);
-
         //child.setY(dependency)
 
         System.out.println("startPictureYPosition = " +child.getY());
@@ -202,13 +188,16 @@ public class ActorAvatarBehavior2 extends CoordinatorLayout.Behavior<CircleImage
         System.out.println("startToolbarYPosition = " +dependency.getY());
 
 
-        startPictureYPosition = dependency.getBottom() - child.getHeight()/2;
+        startPictureYPosition = dependency.getBottom() - child.getHeight()/2 - getStatusBarHeight();
 
 
+
+        mChangeBehaviorPoint = (child.getHeight() - finalPictureSize) / (2f * ((dependency.getBottom() - child.getHeight()) - finalPictureYPosition));
+        System.out.println("mChangeB = " +mChangeBehaviorPoint);
 
         //startPictureYPosition = (int) (dependency.getY() - child.getHeight()/2);
         //startPictureXPosition = (dependency.getHeight()/2 - child.getWidth()/2);
-        //startPictureSize = (int) child.getWidth();
+        startPictureSize = (int) child.getWidth();
 
 
         //changeBehaviorPoint = (child.getHeight() - 0) / (2f * (startPictureYPosition - 0));
@@ -218,11 +207,24 @@ public class ActorAvatarBehavior2 extends CoordinatorLayout.Behavior<CircleImage
         System.out.println("startPictureYPosition = " +startPictureYPosition);
         System.out.println("startPictureXPosition = " +startPictureXPosition);
         System.out.println("startPictureSize = " +startPictureSize);
-        System.out.println("startToolbarYPosition = " +dependency.getY());
 
 
+        startToolbarYPosition = dependency.getBottom();
+        System.out.println("startToolbarYPosition = " +startToolbarYPosition);*/
 
 
         wasInitiated = true;
+    }
+
+    private int getStatusBarHeight() {
+        Rect rectangle = new Rect();
+        Window window = ((DetailsActorActivity) mContext).getWindow();
+        window.getDecorView().getWindowVisibleDisplayFrame(rectangle);
+        int statusBarHeight = rectangle.top;
+        int contentViewTop =
+                window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
+        int titleBarHeight= contentViewTop - statusBarHeight;
+
+        return statusBarHeight;
     }
 }
